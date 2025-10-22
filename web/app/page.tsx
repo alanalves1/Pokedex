74bc8117e39b2f11ec36a5ff43bb7ab50e040fc5
alanalves1 <alan_o_alves@outlook.com>
2025-10-21 22:39:13 -0400

    Exercicio1

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,6 +1,11 @@
+"use client";
 import Image from "next/image";
-
+import React from "react";
+function greetUser(name: string): string {
+  return `Hello, ${name}! `;
+}
 export default function Home() {
+  const [name, setName] = React.useState("alice");
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -14,10 +19,11 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            Hello {name}, welcome to Next.js 13 with the App Router!
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
+            <input type="text" className="border" value={name} onChange={e => setName(e.target.value)} />
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
@@ -58,6 +64,12 @@ export default function Home() {
           >
             Documentation
           </a>
+          <button onClick={() => {
+            const message = greetUser(name);
+            alert(message)
+          }}>
+            say hello
+          </button>
         </div>
       </main>
     </div>
